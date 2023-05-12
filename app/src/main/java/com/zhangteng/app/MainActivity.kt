302139d082bec3624dd7555aa17be36aa2bd4032
Swing <763263311@qq.com>
2023-05-12 21:45:10 +0800

    style: 删除无用demo

--- app/src/main/java/com/zhangteng/app/MainActivity.kt
+++ app/src/main/java/com/zhangteng/app/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.zhangteng.app
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import com.zhangteng.aop.annotation.CheckNet
 import com.zhangteng.aop.annotation.Permissions
 import com.zhangteng.aop.annotation.SingleClick
 import com.zhangteng.aop.annotation.TimeLog
-import com.zhangteng.app.activity.BaseDemoActivity
-import com.zhangteng.app.activity.NineImageActivity
-import com.zhangteng.app.activity.TabLayoutActivity
-import com.zhangteng.app.activity.TreeActivity
+import com.zhangteng.app.activity.BaseMvpDemoActivity
 import com.zhangteng.app.databinding.ActivityMainBinding
 import com.zhangteng.app.mvp.model.imodel.IMainModel
 import com.zhangteng.app.mvp.presenter.ipresenter.IMainPresenter
 import com.zhangteng.app.mvp.view.IMainView
 import com.zhangteng.mvp.mvp.vb.BaseMvpActivity
-import com.zhangteng.utils.StateViewHelper
 import com.zhangteng.utils.jumpToActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -56,21 +51,6 @@ class MainActivity : BaseMvpActivity<ActivityMainBinding, IMainView, IMainModel,
     @SingleClick
     @Permissions(value = ["android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"])
     fun onClickBaseLayout(v: View) {
-        jumpToActivity<BaseDemoActivity>()
-    }
-
-    @TimeLog
-    fun onClickTabLayout(v: View) {
-        jumpToActivity<TabLayoutActivity>()
-    }
-
-    @TimeLog
-    fun onClickTree(v: View) {
-        jumpToActivity<TreeActivity>()
-    }
-
-    @TimeLog
-    fun onClickNineImage(v: View) {
-        jumpToActivity<NineImageActivity>()
+        jumpToActivity<BaseMvpDemoActivity>()
     }
 }
